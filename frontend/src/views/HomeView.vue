5553fc5468fcbd10e874a6b4af6c2b70cd50db5a
Danii <daniel.marin92@hotmail.com>
2024-04-13 19:41:03 -0600

    Implementación de estilos PrimeVue, e iconos

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+
 </script>
 
+
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="example">
+      <InputGroup>
+          <InputGroupAddon>
+              <i class="pi pi-user"></i>
+          </InputGroupAddon>
+          <InputText placeholder="Username" />
+      </InputGroup>
+
+      <InputGroup>
+          <InputGroupAddon>$</InputGroupAddon>
+          <InputNumber placeholder="Price" />
+          <InputGroupAddon>.00</InputGroupAddon>
+      </InputGroup>
+
+      <InputGroup>
+          <InputGroupAddon>www</InputGroupAddon>
+          <InputText placeholder="Website" />
+      </InputGroup>
+  </div>
 </template>
+
+<style scoped lang="css">
+.example {
+  display: flex;
+  gap: 20px;
+  width: 80dvw;
+}
+</style>
