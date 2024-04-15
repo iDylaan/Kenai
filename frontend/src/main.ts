f84d69fce1e28d04bbd68ffccad4c2c68fd96585
Danii <daniel.marin92@hotmail.com>
2024-04-14 23:58:30 -0600

    Ajustes tema de PrimeVue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,7 +6,6 @@ import { createPinia } from 'pinia'
 import PrimeVue from "primevue/config"
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
-import 'primevue/resources/themes/aura-dark-amber/theme.css';
 
 import App from './App.vue'
 import router from './router'
