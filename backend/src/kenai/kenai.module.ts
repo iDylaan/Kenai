cb271436d617508abd99f6284964cdeac24e9f4c
Danii <daniel.marin92@hotmail.com>
2024-05-08 20:51:46 -0600

    Eliminación de backend en NodeJs

--- backend/src/kenai/kenai.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { KenaiService } from './kenai.service';
-import { KenaiController } from './kenai.controller';
-
-@Module({
-  providers: [KenaiService],
-  controllers: [KenaiController]
-})
-export class KenaiModule {}
