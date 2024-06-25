057960e0b9ea18d5fa7f1fba6b91a12f8c98437d
brandon_campos_f <camps.wp@outlook.com>
2024-06-24 20:02:30 -0600

    conexion postgres
    Se agrega conexion con postgres

--- backend/app/app_config.py
+++ backend/app/app_config.py
@@ -1,5 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+
 class Config:
     ENVIROMENT = os.getenv("ENVIROMENT")
+
+    ### POSTGRESQL ###
+    PGL_HOST = os.getenv("PGL_HOST")
+    PGL_USER = os.getenv("PGL_USER")
+    PGL_PASS = os.getenv("PGL_PASS")
+    PGL_PORT = os.getenv("PGL_PORT")
+    PGL_DB = os.getenv("PGL_DB")
